51980ef68956eab6e311b2650bf5b960d71dbb4d
Anish Sana <anishsana91@gmail.com>
2017-04-01 19:28:43 -0400

    re-checked code from source

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-var http = require('http');
-var mongoose = require('mongoose');
-var express = require('express');
+var http      = require('http');
+var mongoose  = require('mongoose');
+var express   = require('express');
 
-var app = express();
+
+var app    = express();
 var db;
 
 var config = {
-	"USER" : "",
-	"PASS" : "",
-	"HOST" : "ec2-54-86-98-101.compute-1.amazonaws.com",
-	"PORT" : "27017",
+	"USER"    : "",           
+	"PASS"    : "",
+	"HOST"    : "ec2-54-86-98-101.compute-1.amazonaws.com",  
+	"PORT"    : "27017", 
 	"DATABASE" : "my_example"
 };
 
-var dbPath = "mongodb://" + config.USER + ":" + config.PASS + "@" + config.HOST + ":" + config.PORT + "/" + config.DATABASE;
-
+var dbPath  = "mongodb://"+config.USER + ":"+
+config.PASS + "@"+
+config.HOST + ":"+
+config.PORT + "/"+
+config.DATABASE;
 var standardGreeting = 'Hello World!';
 
 var greetingSchema = mongoose.Schema({
 	sentence: String
-});
-var Greeting = mongoose.model('Greeting', greetingSchema);
+}); 
+var Greeting= mongoose.model('Greeting', greetingSchema);
 
 db = mongoose.connect(dbPath);
 
 mongoose.connection.once('open', function() {
 	var greeting;
-	Greeting.find(function(err, greetings) {
-		if(!greetings) {
-			greeting = new Greeting({sentence: standardGreeting});
+	Greeting.find( function(err, greetings){
+		if( !greetings ){     
+			greeting = new Greeting({ sentence: standardGreeting }); 
 			greeting.save();
-		}
-	});
+		} 
+	}); 
 });
 
-app.get('/', function(req, res) {
-	Greeting.findOne(function(err, greeting) {
+app.get('/', function(req, res){
+	Greeting.findOne(function (err, greeting) {
 		res.send(greeting.sentence);
 	});
 });
 
-app.use(function(err, req, res, next) {
-	if(req.xhr) {
+app.use(function(err, req, res, next){
+	if (req.xhr) {
 		res.send(500, 'Something went wrong!');
-	} else {
+	}
+	else {
 		next(err);
 	}
 });
 
-console.log('starting the Express (NodeJS) Web Server');
+console.log('starting the Express (NodeJS) Web server');
 app.listen(8080);
 console.log('Webserver is listening on port 8080');
